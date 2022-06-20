9259f7c4e86f5824a9f4c5b8e1250b0619ff6b2d
ligusta <lushnikov7294@gmail.com>
2022-06-21 02:05:10 +0300

    add JS script

---
+++ script.js
@@ -0,0 +1,12 @@
+let star = document.getElementById('img__stars')
+let moon = document.getElementById('img__moon')
+let mountainBehind = document.getElementById('img__mountain-behind')
+let mountainFront = document.getElementById('img__mountain-front')
+let moonText = document.getElementById('moon__text')
+let moonBtn = document.getElementById('moon__button')
+let navbar = document.getElementsByClassName('navbar')
+
+window.addEventListener('scroll', function()) {
+    let scrollPos = window.scrollY 
+    console.log(scrollPos)
+}
