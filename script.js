d4ba733dea9a3f5246306bd24dbf1d2f8863d28c
ligusta <lushnikov7294@gmail.com>
2022-06-21 23:47:51 +0300

    fix animation and add active button

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let mountainBehind = document.getElementById('img__mountain-behind');
 let mountainFront = document.getElementById('img__mountain-front');
 let moonText = document.getElementById('moon__text');
 let moonBtn = document.getElementById('moon__button');
-let navbar = document.getElementsByClassName('navbar');
 
 window.addEventListener('scroll', function() {
     let scrollPos = window.scrollY;
@@ -14,6 +13,5 @@ window.addEventListener('scroll', function() {
     moon.style.top = scrollPos * 0.75 + 'px';
     mountainBehind.style.top = scrollPos * 0.25 + 'px';
     moonText.style.paddingRight = scrollPos * 2 + 'px';
-    moonBtn.style.left = scrollPos * 2 + 'px';
-    navbar.style.top = scrollPos * 0.25 + 'px';
+    moonBtn.style.marginRight = scrollPos * 1.2 + 'px';
 })
